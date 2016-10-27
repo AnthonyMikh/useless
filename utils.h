f7950c6845739e562f19ba24f0a799868f066814
AnthonyMikh <hedgehog1024@yandex.ru>
2016-10-27 21:26:43 +0400

    Put all utilities functions in separated file

---
+++ utils.h
@@ -0,0 +1,5 @@
+size_t countlines(FILE* file);
+void allocate_arr(entry** arr, size_t countline);
+void readentries(FILE* file, entry* arr, size_t countline);
+void sortentries(entry* arr, size_t countline);
+void execute(char* progname);
