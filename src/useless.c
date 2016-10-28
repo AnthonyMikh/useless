cebf0f4eb86a0ff930a0449d1f5759bfbd6b964e
AnthonyMikh <hedgehog1024@yandex.ru>
2016-10-28 12:34:33 +0400

    Styling change

--- src/useless.c
+++ src/useless.c
@@ -30,7 +30,7 @@ int main (int argc, char** argv)
     }
 
     FILE *file = fopen(argv[1],"r");
-    if (file == 0)
+    if (file == NULL)
     {
         fprintf(stderr, "Error occured trying to open file.\n");
         return -1;
