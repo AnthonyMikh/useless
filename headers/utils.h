005b76781ba221994e286af7293895e6d5ebcad3
AnthonyMikh <hedgehog1024@yandex.ru>
2016-10-28 12:31:19 +0400

    Update utils.h

--- headers/utils.h
+++ headers/utils.h
@@ -5,4 +5,4 @@ void allocate_arr(entry** arr, size_t countline);
 void readentries(FILE* file, entry* arr, size_t countline);
 void sortentries(entry* arr, size_t countline);
 void execute(char* progname);
-int min (int a, int b);
+int max (int a, int b);
